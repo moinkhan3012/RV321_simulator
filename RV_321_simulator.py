34157fe656e00a513bf2a1b9113e0aeedb5e1e31
moinkhan3012@gmail.com <Moin Khan>
2023-03-12 22:34:22 -0400

    Removed unnecessary codes

--- RV_321_simulator.py
+++ RV_321_simulator.py
@@ -8,8 +8,8 @@ from riscvmodel.code import decode, MachineDecodeError
 from riscvmodel.isa import Instruction
 from bitstring import BitArray
 
-MemSize = 1000
-# TODO: set nop default to false and handle it in init for core class
+MemSize = 1000 # memory size, in reality, the memory size should be 2^32, but for this lab, for the space resaon, we keep it as this large number, but the memory is still 32-bit addressable.
+
 class InsMem(object):
 
     def __init__(self, name, ioDir):
@@ -18,7 +18,7 @@ class InsMem(object):
         with open(ioDir + "/imem.txt") as im:
             self.IMem = [data.replace("\n", "") for data in im.readlines()]
 
-    def read_instr(self, read_address: int):
+    def read_instr(self, read_address):
         # DONE: Handle word addressing - use nearest lower multiple for 4 for address = x - x % 4
         read_address = read_address - read_address % 4
         if len(self.IMem) < read_address + 4:
@@ -33,7 +33,7 @@ class DataMem(object):
         with open(ioDir + "\\dmem.txt") as dm:
             self.DMem = [data.replace("\n", "") for data in dm.readlines()]
 
-    def read_data(self, read_address: int) -> int:
+    def read_data(self, read_address):
         # read data memory
         # return 32-bit signed int value
 
@@ -43,7 +43,7 @@ class DataMem(object):
             raise Exception("Data MEM - Out of bound access")
         return BitArray(bin="".join(self.DMem[read_address: read_address + 4])).int32
 
-    def write_data_mem(self, address: int, write_data: int):
+    def write_data_mem(self, address, write_data):
         # write data into byte addressable memory
         # Assuming data as 32 bit signed integer
 
@@ -76,25 +76,22 @@ class RegisterFile(object):
         self.outputFile = ioDir + "RFResult.txt"
         self.Registers = [0x0 for i in range(32)]
 
-    def read_rf(self, reg_addr: int) -> int:
+    def read_rf(self, reg_addr):
         return self.Registers[reg_addr]
 
-    def write_rf(self, reg_addr: int, wrt_reg_data: int):
+    def write_rf(self, reg_addr, wrt_reg_data):
         if reg_addr != 0:
             self.Registers[reg_addr] = wrt_reg_data
 
     def output_rf(self, cycle):
         op = ["State of RF after executing cycle:\t" + str(cycle) + "\n"]
         op.extend(['{:032b}'.format(val & 0xffffffff) + "\n" for val in self.Registers])
-        if cycle == 0:
-            perm = "w"
-        else:
-            perm = "a"
+        if cycle == 0:perm = "w"
+        else: perm = "a"
         with open(self.outputFile, perm) as file:
             file.writelines(op)
 
 class State(object):
-
     def __init__(self):
         self.IF = {"nop": False, "PC": 0, "instruction_count":0}
         self.ID = {"nop": False, "Instr": 0}
@@ -103,13 +100,6 @@ class State(object):
         self.MEM = {"nop": False, "ALUresult": 0, "Store_data": 0, "Rs": 0, "Rt": 0, "Wrt_reg_addr": 0, "rd_mem": 0, 
                    "wrt_mem": 0, "wrt_enable": 0}
         self.WB = {"nop": False, "Wrt_data": 0, "Rs": 0, "Rt": 0, "Wrt_reg_addr": 0, "wrt_enable": 0}
-
-    def nop_init(self):
-        self.IF['nop'] = False
-        self.ID['nop'] = True
-        self.EX['nop'] = True
-        self.MEM['nop'] = True
-        self.WB['nop'] = True
 
     def __str__(self):
         # DONE: update __str__ to make use of individual State objects
@@ -125,20 +115,14 @@ class InstructionBase():
         self.state = state
         self.nextState = nextState
 
-    def mem_ss(self, *args, **kwargs):
-        pass
-
-    def wb_ss(self, *args, **kwargs):
-        pass
-
     def execute(self, *args, **kwargs):
-        return self.execute_ss(*args, **kwargs)
+        pass
 
     def mem(self, *args, **kwargs):
-        return self.mem_ss(*args, **kwargs)
+        pass
 
     def wb(self, *args, **kwargs):
-        return self.wb_ss(*args, **kwargs)
+        pass
 
 class InstructionRBase(InstructionBase):
     def __init__(self, instruction: Instruction, memory: DataMem, registers: RegisterFile, state: State,
@@ -148,7 +132,7 @@ class InstructionRBase(InstructionBase):
         self.rs2 = instruction.rs2
         self.rd = instruction.rd
 
-    def wb_ss(self, *args, **kwargs):
+    def wb(self, *args, **kwargs):
         data = kwargs['alu_result']
         return self.registers.write_rf(self.rd, data)
 
@@ -160,7 +144,7 @@ class InstructionIBase(InstructionBase):
         self.rd = instruction.rd
         self.imm = instruction.imm.value
 
-    def wb_ss(self, *args, **kwargs):
+    def wb(self, *args, **kwargs):
         data = kwargs['alu_result']
         return self.registers.write_rf(self.rd, data)
 
@@ -172,7 +156,7 @@ class InstructionSBase(InstructionBase):
         self.rs2 = instruction.rs2
         self.imm = instruction.imm.value
 
-    def mem_ss(self, *args, **kwargs):
+    def mem(self, *args, **kwargs):
         address = kwargs['alu_result']
         data = self.registers.read_rf(self.rs2)
         self.memory.write_data_mem(address, data)
@@ -188,7 +172,7 @@ class InstructionBBase(InstructionBase):
     def take_branch(self, operand1, operand2):
         pass
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         pass
 
 class InstructionJBase(InstructionBase):
@@ -198,7 +182,7 @@ class InstructionJBase(InstructionBase):
         self.rd = instruction.rd
         self.imm = instruction.imm.value
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         pass
 
 class ADD(InstructionRBase):
@@ -206,34 +190,31 @@ class ADD(InstructionRBase):
                  nextState: State):
         super(ADD, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) + self.registers.read_rf(self.rs2)
 
 class SUB(InstructionRBase):
-
     def __init__(self, instruction: Instruction, memory: DataMem, registers: RegisterFile, state: State,
                  nextState: State):
         super(SUB, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) - self.registers.read_rf(self.rs2)
 
 class XOR(InstructionRBase):
-
     def __init__(self, instruction: Instruction, memory: DataMem, registers: RegisterFile, state: State,
                  nextState: State):
         super(XOR, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) ^ self.registers.read_rf(self.rs2)
 
 class OR(InstructionRBase):
-
     def __init__(self, instruction: Instruction, memory: DataMem, registers: RegisterFile, state: State,
                  nextState: State):
         super(OR, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) | self.registers.read_rf(self.rs2)
 
 class AND(InstructionRBase):
@@ -241,7 +222,7 @@ class AND(InstructionRBase):
                  nextState: State):
         super(AND, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) & self.registers.read_rf(self.rs2)
 
 class ADDI(InstructionIBase):
@@ -249,7 +230,7 @@ class ADDI(InstructionIBase):
                  nextState: State):
         super(ADDI, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) + self.imm
 
 class XORI(InstructionIBase):
@@ -257,16 +238,15 @@ class XORI(InstructionIBase):
                  nextState: State):
         super(XORI, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) ^ self.imm
 
 class ORI(InstructionIBase):
-
     def __init__(self, instruction: Instruction, memory: DataMem, registers: RegisterFile, state: State,
                  nextState: State):
         super(ORI, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) | self.imm
 
 class ANDI(InstructionIBase):
@@ -274,7 +254,7 @@ class ANDI(InstructionIBase):
                  nextState: State):
         super(ANDI, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) & self.imm
 
 class LW(InstructionIBase):
@@ -282,14 +262,14 @@ class LW(InstructionIBase):
                  nextState: State):
         super(LW, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) + self.imm
 
-    def mem_ss(self, *args, **kwargs):
+    def mem(self, *args, **kwargs):
         address = kwargs['alu_result']
         return self.memory.read_data(address)
 
-    def wb_ss(self, *args, **kwargs):
+    def wb(self, *args, **kwargs):
         data = kwargs['mem_result']
         return self.registers.write_rf(self.rd, data)
 
@@ -298,12 +278,10 @@ class SW(InstructionSBase):
                  nextState: State):
         super(SW, self).__init__(instruction, memory, registers, state, nextState)
 
-    def execute_ss(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         return self.registers.read_rf(self.rs1) + self.imm
 
-
 class BEQ(InstructionBBase):
-
     def __init__(self, instruction: Instruction, memory: DataMem, registers: RegisterFile, state: State,
                  nextState: State):
         super(BEQ, self).__init__(instruction, memory, registers, state, nextState)
@@ -311,9 +289,7 @@ class BEQ(InstructionBBase):
     def take_branch(self, operand1, operand2):
         return operand1 == operand2
 
-
 class BNE(InstructionBBase):
-
     def __init__(self, instruction: Instruction, memory: DataMem, registers: RegisterFile, state: State,
                  nextState: State):
         super(BNE, self).__init__(instruction, memory, registers, state, nextState)
@@ -321,13 +297,10 @@ class BNE(InstructionBBase):
     def take_branch(self, operand1, operand2):
         return operand1 != operand2
 
-
 class JAL(InstructionJBase):
-
     def __init__(self, instruction: Instruction, memory: DataMem, registers: RegisterFile, state: State,
                  nextState: State):
         super(JAL, self).__init__(instruction, memory, registers, state, nextState)
-
 
 class ADDERBTYPE:
     def __init__(self, instruction: Instruction, state: State(), registers: RegisterFile):
@@ -372,12 +345,6 @@ def get_instruction_class(mnemonic):
         return cls
     except AttributeError as e:
         raise Exception("Invalid Instruction")
-    
-
-#rv32i
-
-# memory size, in reality, the memory size should be 2^32, but for this lab, for the space reason
-# we keep it as this large number, but the memory is still 32-bit addressable.
 
 class Core(object):
     def __init__(self, ioDir: str, imem: InsMem, dmem: DataMem):
@@ -386,9 +353,7 @@ class Core(object):
         self.halted = False
         self.ioDir = ioDir
         self.state = State()
-        self.state.nop_init()
         self.nextState = State()
-        self.nextState.nop_init()
         self.ext_imem: InsMem = imem
         self.ext_dmem: DataMem = dmem
 
@@ -396,22 +361,19 @@ class Core(object):
         cpi = float(self.cycle) / self.state.IF['instruction_count']
         ipc = 1 / cpi
 
-        result_format = f"{self.stages} Core Performance Metrics-----------------------------\n" \
+        result_format = f"{'-'*15} Core Performance Metrics {'-'*15}\n" \
                         f"Number of cycles taken: {self.cycle}\n" \
                         f"Cycles per instruction: {cpi}\n" \
                         f"Instructions per cycle: {ipc}\n"
 
-        write_mode = "w" if self.stages == "Single Stage" else "a"
-
         print(self.ioDir[:-3] + "PerformanceMetrics_Result.txt")
-        with open(self.ioDir[:-3] + "PerformanceMetrics_Result.txt", write_mode) as file:
+        with open(self.ioDir[:-3] + "PerformanceMetrics_Result.txt", 'w') as file:
             file.write(result_format)
 
 class SingleStageCore(Core):
     def __init__(self, ioDir: str, imem: InsMem, dmem: DataMem):
         super(SingleStageCore, self).__init__(ioDir + "/SS_", imem, dmem)
         self.opFilePath = ioDir + "/StateResult_SS.txt"
-        self.stages = "Single Stage"
 
     def step(self):
         # IF
@@ -463,10 +425,8 @@ class SingleStageCore(Core):
         printstate.append("IF.PC: " + str(state.IF['PC']) + "\n")
         printstate.append("IF.nop: " + str(state.IF['nop']) + "\n")
 
-        if (cycle == 0):
-            perm = "w"
-        else:
-            perm = "a"
+        if (cycle == 0):perm = "w"
+        else: perm = "a"
         with open(self.opFilePath, perm) as wf:
             wf.writelines(printstate)
 
